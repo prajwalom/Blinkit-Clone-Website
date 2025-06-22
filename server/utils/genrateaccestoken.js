b0614806e858d4ea0d2b6ba870ee3c5695919953
prajwalom <prajwalpadole99@gmail.com>
2025-06-22 11:11:59 +0530

    Add generateAccessToken utility for JWT token generation

---
+++ server/utils/genrateaccestoken.js
@@ -0,0 +1,9 @@
+const generateAccessToken = async (userId) => {
+    const token = await jwt.sign( {id : userId}, process.env.SECRET_KEY_ACCESS_TOKEN, {
+        expiresIn: '1d'
+    })
+
+    return token;
+}
+
+export default generateAccessToken;
