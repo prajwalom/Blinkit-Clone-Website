20735516b42cca0d5f1d4b19f027333f2d79dc43
prajwalom <prajwalpadole99@gmail.com>
2025-06-22 11:16:04 +0530

    Add generateRefreshToken utility for JWT refresh token generation

---
+++ server/utils/genrateRefreshToken.js
@@ -0,0 +1,15 @@
+const genrateRefreshToken = async (userId) => {
+    const token = jwt.sign({ id: userId }, process.env.SECRET_KEY_REFRESH_TOKEN, {
+        expiresIn: '30d'
+    });
+
+    const updateRefreshToken = await User.updateone ({ _id: userId }, 
+        {
+        refresh_token: token
+        }
+    );
+
+    return token;
+}
+
+export default genrateRefreshToken;
